a5a0cc6e4c4d1783bd09b9dca97461e1c00a1d0f
Charles Ofria <charles.ofria@gmail.com>
2023-01-21 12:33:06 -0500

    Setup Emperfect executable to use command line argument.

--- src/Emperfect.cpp
+++ src/Emperfect.cpp
@@ -7,4 +7,12 @@
 int main(int argc, char *argv[])
 {
   std::cout << "Welcome to Emperfect!" << std::endl;
+
+  if (argc != 2) {
+    std::cout << "Format: " << argv[0] << " [config filename]" << std::endl;
+    exit(1);
+  }
+
+  Emperfect control;
+  control.Load(argv[1]);
 }
