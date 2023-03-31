8fd68ae31741cbabf4519048f15ee69aba92705a
Charles Ofria <charles.ofria@gmail.com>
2023-03-31 08:51:23 -0400

    If non-zero exit code was not expected, must be failed run.

--- src/Testcase.hpp
+++ src/Testcase.hpp
@@ -95,7 +95,7 @@ public:
     if (hit_timeout) return TestStatus::FAILED_TIME;
     if (run_exit_code != expect_exit_code) {
       if (expect_exit_code) return TestStatus::MISSED_ERROR;
-      if (run_exit_code) return TestStatus::FAILED_RUN;
+      return TestStatus::FAILED_RUN;
     }
     if (CountPassed() != checks.size()) return TestStatus::FAILED_CHECK;
     if (!output_match) return TestStatus::FAILED_OUTPUT;
